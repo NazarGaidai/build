0af4363f52b77c5e35b073e9cd63a0bb999a474e
TELEPUZIK-DIPSI <164372468+TELEPUZIK-DIPSI@users.noreply.github.com>
2024-03-24 02:38:36 +0100

    Update pagination.js

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const response = await fetch('#');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await response.json();
   return data;
 }
