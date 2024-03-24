b56095fdfdd7b717dcdecbaae4a3d7dc4f3ced6c
Nazar <nazargaidai8@gmail.com>
2024-03-24 23:57:28 +0200

    1

--- js/sorted-menu.js
+++ js/sorted-menu.js
@@ -1,5 +1,25 @@
-let sorted = document.querySelector('#sorted');
-// console.log(sorted);
-replaceNode = sorted.replaceChild(sorted.children[1], sorted.children[0]);
-console.log(replaceNode);
-sorted.appendChild(replaceNode);
+// let sorted = document.querySelector('#sorted');
+// // console.log(sorted);
+// replaceNode = sorted.replaceChild(sorted.children[1], sorted.children[0]);
+// console.log(replaceNode);
+// sorted.appendChild(replaceNode);
+
+document.querySelector('#sort-prisehi').onclick = mySort;
+
+function mySort() {
+    let sorted = document.querySelector('#sorted');
+    for (let i = 0; i < sorted.children.length; i++) {
+       
+        for (let j = i; j < sorted.children.length; j++){
+           if(+sorted.children[i].getAttribute('data-sort') > +sorted.children[j].getAttribute('data-sort')){
+            replaceNode = sorted.replaceChild(sorted.children[j], sorted.children[i]);
+            insertAfter(replaceNode, sorted.children[i]);
+           }
+        }
+        
+    }
+}
+
+function insertAfter(elem, refElem){
+    return refElem.parentNode.insertBefore(elem, refElem.mextSibling);
+}
