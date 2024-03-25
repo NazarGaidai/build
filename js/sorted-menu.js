f5bc9586f14c13b810429ca94e1cfe47563313d4
TELEPUZIK-DIPSI <164372468+TELEPUZIK-DIPSI@users.noreply.github.com>
2024-03-25 22:39:50 +0100

    working kategories
    nazar xujlusa

--- js/sorted-menu.js
+++ js/sorted-menu.js
@@ -1,9 +1,3 @@
-// let sorted = document.querySelector('#sorted');
-// // console.log(sorted);
-// replaceNode = sorted.replaceChild(sorted.children[1], sorted.children[0]);
-// console.log(replaceNode);
-// sorted.appendChild(replaceNode);
-
 document.querySelector('#sort-hi').onclick = mySort;
 
 function mySort() {
@@ -11,8 +5,8 @@ function mySort() {
     for (let i = 0; i < sorted.children.length; i++) {
        
         for (let j = i; j < sorted.children.length; j++){
-           if(+sorted.children[i].getAttribute('data-sort') > +sorted.children[j].getAttribute('data-sort')){
-            replaceNode = sorted.replaceChild(sorted.children[j], sorted.children[i]);
+           if(parseInt(sorted.children[i].getAttribute('data-sort')) > parseInt(sorted.children[j].getAttribute('data-sort'))){
+            let replaceNode = sorted.replaceChild(sorted.children[j], sorted.children[i]);
             insertAfter(replaceNode, sorted.children[i]);
            }
         }
@@ -21,5 +15,5 @@ function mySort() {
 }
 
 function insertAfter(elem, refElem){
-    return refElem.parentNode.insertBefore(elem, refElem.mextSibling);
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
 }
